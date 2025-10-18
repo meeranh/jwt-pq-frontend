e497b77850e51ffb6284bb64116f96314b578128
meeranh <themeeranhassan@gmail.com>
2025-10-18 20:09:14 +0530

    fix: fixed cloudflare adapter issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: false
-    }),
-    prerender: {
-      handleHttpError: ({ path, referrer, message }) => {
-        // Ignore favicon errors
-        if (path === '/favicon.png' || path === '/favicon.ico') {
-          return;
-        }
-        throw new Error(message);
+      // Cloudflare Pages configuration
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
       }
-    }
+    })
   }
 };
 
